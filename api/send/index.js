433219333d9e18dfb6810635c372281e064fa262
言葉 <funaox@gmail.com>
2022-08-01 23:34:19 +0900

    Update index.js

--- api/send/index.js
+++ api/send/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json({ type: '*/*' }))
+
 import webpush from 'web-push'
 
 const defaultVapidDetails = {
