6b0bd0d561fd83b0a009b9c8124ad79387e5dd71
言葉 <funaox@gmail.com>
2022-08-01 21:59:01 +0900

    Update index.js

--- api/send/index.js
+++ api/send/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.json({type: '*/*'}))
+app.use(express.json({ type: '*/*' }))
 const webpush = require('web-push')
 
 app.get('/api/send', async (req, res) => {
