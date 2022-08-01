446e5c85be51e333edcd558e4b05c3042cfa1279
言葉 <funaox@gmail.com>
2022-08-01 17:19:39 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-// Export the Express API
 module.exports = app;
