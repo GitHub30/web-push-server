85bd3e04ae5003861c6618948cca3e3c67a65403
言葉 <funaox@gmail.com>
2022-08-02 02:35:07 +0900

    Update index.js

--- api/send/index.js
+++ api/send/index.js
@@ -12,13 +12,9 @@ const defaultVapidDetails = {
   privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI',
 }
 
-const send = o => {
-  const options = {
-    vapidDetails: o.vapidDetails || defaultVapidDetails
-  }
-  if (o.gcmAPIKey) options.gcmAPIKey = o.gcmAPIKey
-  return webpush.sendNotification(o.subscription, o.payload || 'Hello', options)
-}
+const send = o => webpush.sendNotification(o.subscription, o.payload || 'Hello', {
+  vapidDetails: o.vapidDetails || defaultVapidDetails
+})
 
 app.get('/api/send', async (req, res) => {
   if (req.query.json) res.json(await send(JSON.parse(req.query.json)))
