da4082d7e45bbea1bf3ba1e38d5982e8110f1989
言葉 <funaox@gmail.com>
2022-08-01 22:31:08 +0900

    Update index.js

--- api/send/index.js
+++ api/send/index.js
@@ -3,6 +3,12 @@ const app = express()
 app.use(express.json({ type: '*/*' }))
 const webpush = require('web-push')
 
+const defaultVapidDetails = {
+  subject: 'mailto:example@yourdomain.org',
+  publicKey: 'BDd3_hVL9fZi9Ybo2UUzA284WG5FZR30_95YeZJsiApwXKpNcF1rRPF3foIiBHXRdJI2Qhumhf6_LFTeZaNndIo',
+  privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI',
+}
+
 app.get('/api/send', async (req, res) => {
   // VAPID keys should be generated only once.
   const vapidKeys = {
@@ -34,7 +40,11 @@ app.get('/api/send', async (req, res) => {
 app.post('/api/send', async (req, res) => {
   console.log(req.body)
   // https://github.com/web-push-libs/web-push/blob/master/README.md#sendnotificationpushsubscription-payload-options
-  res.json(await webpush.sendNotification(req.body.subscription, req.body.payload, req.body))
+  const options = {
+    vapidDetails: req.body.vapidDetails || defaultVapidDetails
+  }
+  if (req.body.gcmAPIKey) options.gcmAPIKey = req.body.gcmAPIKey
+  res.json(await webpush.sendNotification(req.body.subscription, req.body.payload || 'Hello', options))
 })
 
 module.exports = app;
